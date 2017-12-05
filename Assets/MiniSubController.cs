70a3c662d2159bd186a4794abc3180de2038c13e
unknown <usrlocal@GA211-FE18.ad.univ-angers.fr>
2017-12-05 15:58:53 +0100

    Now rotate the submarine

--- Assets/MiniSubController.cs
+++ Assets/MiniSubController.cs
@@ -7,6 +7,8 @@ public class MiniSubController : MonoBehaviour {
     [SerializeField] private GameObject cam;
     private Rigidbody r;
     [SerializeField] private float speed = 4f;
+    [SerializeField] private float step = .1f;
+
     private float rtSpeed;
     Vector3 dirForward, dirStrafe;
 
@@ -17,18 +19,18 @@ public class MiniSubController : MonoBehaviour {
 
     // Update is called once per frame
     void Update () {
-        //float step = Time.deltaTime * speed;
 
         rtSpeed = speed;
-
         if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
-            rtSpeed *= 2;
+            rtSpeed *= 4;
 
         dirForward = (transform.position - cam.transform.position).normalized;
 
         r.AddForce(50 * rtSpeed * dirForward * Input.GetAxis("Vertical"));
 
-        //Vector3.RotateTowards(submarine.transform.rotation, dirForward, step, 0.0F);
+        Vector3 dir = Vector3.RotateTowards(transform.forward, cam.transform.forward, step, 0.0F);
+
+        transform.rotation = Quaternion.LookRotation(dir);
 
         //r.AddForce(100 * Vector3.right * Input.GetAxis("Horizontal"));
     }
