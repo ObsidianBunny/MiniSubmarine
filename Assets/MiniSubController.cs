50b116d51ce0fce2f3e1aae58a6b6bc28371639b
unknown <usrlocal@GA211-FE18.ad.univ-angers.fr>
2017-12-05 14:03:40 +0100

    Added Submarine Controller

---
+++ Assets/MiniSubController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniSubController : MonoBehaviour {
+
+    [SerializeField] private GameObject cam;
+    private Rigidbody r;
+    [SerializeField] private float speed = 2f;
+    Vector3 dirForward, dirStrafe;
+
+    private void Start()
+    {
+        r = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        float step = Time.deltaTime * speed;
+        dirForward = transform.position - cam.transform.position.normalized;
+
+        r.AddForce(100 * dirForward * Input.GetAxis("Vertical"));
+
+        //Vector3.RotateTowards(submarine.transform.rotation, dirForward, step, 0.0F);
+
+        //r.AddForce(100 * Vector3.right * Input.GetAxis("Horizontal"));
+    }
+}
