13c2a0d439ae9c83f62b7197290a81d5e5c19fb6
unknown <usrlocal@GA211-FE18.ad.univ-angers.fr>
2017-12-05 17:29:08 +0100

    Up/Down Movements

--- Assets/MiniSubController.cs
+++ Assets/MiniSubController.cs
@@ -7,7 +7,7 @@ public class MiniSubController : MonoBehaviour {
     [SerializeField] private GameObject cam;
     private Rigidbody r;
     [SerializeField] private float speed = 4f;
-    [SerializeField] private float step = .01f;
+    [SerializeField] private float step = .1f;
 
     private float rtSpeed;
     Vector3 dirForward, dirStrafe;
@@ -15,19 +15,22 @@ public class MiniSubController : MonoBehaviour {
     private void Start()
     {
         r = GetComponent<Rigidbody>();
+        rtSpeed = speed;
     }
 
     // Update is called once per frame
     void Update () {
+        if ((Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift)) & rtSpeed < 4*speed)
+            rtSpeed *= 1.05f;
+        if (Input.GetKeyUp(KeyCode.LeftShift) || Input.GetKeyUp(KeyCode.RightShift))
+            rtSpeed = speed;
 
-        rtSpeed = speed;
-        if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
-            rtSpeed *= 4;
 
         dirForward = (transform.position - cam.transform.position).normalized;
 
         r.AddForce(50 * rtSpeed * dirForward * Input.GetAxis("Vertical"));
         r.AddForce(30 * rtSpeed * transform.right * Input.GetAxis("Horizontal"));
+        r.AddForce(30 * rtSpeed * transform.up * Input.GetAxis("UpDown"));
 
         Vector3 dir = Vector3.RotateTowards(transform.forward, cam.transform.forward, step, 0.0F);
 
