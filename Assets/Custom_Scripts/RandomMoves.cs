322394ca531e97c4e265dc70508875adb7d768b0
unknown <usrlocal@GA211-FE05.ad.univ-angers.fr>
2017-12-06 12:20:04 +0100

    tentative d'amélioration de la navigation des poissons (échec)

--- Assets/Custom_Scripts/RandomMoves.cs
+++ Assets/Custom_Scripts/RandomMoves.cs
@@ -6,7 +6,9 @@ public class RandomMoves : MonoBehaviour
 {
     public float vitesse_translation;
     public float vitesse_rotation;
-    float angle;
+    private float angleX, angleY;
+    private Vector3 targetDir;
+    private Vector3 newDir;
 
     Collider collisionneur;
 
@@ -22,43 +24,59 @@ public class RandomMoves : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
         //Ray ray = new Ray(transform.position, transform.forward);
 
-        //Debug.Log(collisionneur.Raycast(ray, out hit, 200.00f));
+
         //Debug.DrawRay(this.transform.position, transform.forward*50);
 
         fwd = transform.TransformDirection(Vector3.forward);
 
         //collisionneur.Raycast(ray, out hit, 200.00f))
-
-        if (Physics.Raycast(this.transform.position, fwd, out hit)) 
+        Debug.Log(Physics.Raycast(this.transform.position, fwd, out hit, 100.0f));
+        if (Physics.Raycast(this.transform.position, fwd, out hit, 100.0f))
         {
-            if (hit.distance < 200.0f) {
-                Vector3 newPosition = Random.insideUnitCircle * 5;
-                angle = Mathf.Lerp(angle, Random.Range(-30.0f, 30.0f), Time.deltaTime);
-                //transform.position.Set(newPosition.x, newPosition.y, newPosition.z);
-                transform.Rotate(0, vitesse_rotation * angle * Time.deltaTime, 0);
-                transform.Rotate(vitesse_rotation * angle * Time.deltaTime, 0, 0);
-            }
+            
+            // if (hit.distance < 150.0f) {
+            //Vector3 newPosition = Random.insideUnitCircle * 5;
+            angleX = Mathf.Lerp(angleX, Random.Range(-180.0f, 180.0f), Time.deltaTime);
+            angleY = Mathf.Lerp(angleY, Random.Range(-50.0f, 50.0f), Time.deltaTime);
+            //transform.position.Set(newPosition.x, newPosition.y, newPosition.z);
+            //var desiredRot = Quaternion.Euler(new Vector3(transform.eulerAngles.x + angleX * Time.deltaTime, transform.eulerAngles.y + angleY * Time.deltaTime,0));
+            //Debug.Log(angleX + ": angleX "+angleY+": AngleY "+desiredRot);
+            //transform.rotation = Quaternion.LookRotation(new Vector3(vitesse_rotation * angleX * Time.deltaTime, vitesse_rotation * angleY * Time.deltaTime, 0));
+            //transform.rotation = Quaternion.Lerp(transform.rotation, desiredRot, vitesse_translation * Time.deltaTime);
+            transform.Rotate(0, vitesse_rotation * angleY * Time.deltaTime, 0);
+
+            // }
         }
+
+        transform.Translate(Vector3.forward * vitesse_translation * Time.deltaTime);
+
 
         /*if (transform.rotation.z != 0)
         {
 
             if (transform.rotation.z < 0)
             {
-                angle = Mathf.Lerp(angle, Random.Range(10.0f, 0), Time.deltaTime);
+                angle = Mathf.Lerp(angle, Random.Range(10.0f, 5.0f), Time.deltaTime);
             }
             else
             {
                 angle = Mathf.Lerp(angle, Random.Range(-10.0f, 0), Time.deltaTime);
             }
             transform.Rotate(0, 0, vitesse_rotation * angle * Time.deltaTime);
+            targetDir = new Vector3(0, 0, 0);
+            float step = vitesse_translation * Time.deltaTime;
+            newDir = Vector3.RotateTowards(transform.forward, targetDir, step, 0.0F);
+            transform.rotation = Quaternion.LookRotation(newDir);
+            Quaternion q = transform.rotation;
+            q.eulerAngles = new Vector3(q.eulerAngles.x, q.eulerAngles.y, 0);
+            transform.rotation = q;
         }*/
 
-        transform.Translate(Vector3.forward * vitesse_translation * Time.deltaTime);
-        
+
+
 
     }
 }
