25971a41c4c7b7a43450648b95c4121e799df746
unknown <usrlocal@GA211-FE18.ad.univ-angers.fr>
2017-12-18 13:43:45 +0100

    Scene + suimono modif mineurs

---
+++ Assets/UWeffect.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UWeffect : MonoBehaviour {
+
+    [SerializeField] private GameObject blurPanel;
+	
+	// Update is called once per frame
+	void Update () {
+        if (transform.position.y > 250)
+            blurPanel.SetActive(false);
+        else
+            blurPanel.SetActive(true);		
+	}
+}
---
+++ Assets/ViewChanger.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViewChanger : MonoBehaviour {
+
+    public bool firstPerson = false;
+	
+	// Update is called once per frame
+	void Update () {
+        //if(Input.GetAxis("Fire2") == 1)
+        if (Input.GetKeyDown("joystick button 9"))
+        {
+            Debug.Log("if ok");
+            StartCoroutine("SwitchView");
+        }
+		
+	}
+
+    private IEnumerator SwitchView()
+    {
+        if (firstPerson)
+        {
+            
+            transform.localPosition = new Vector3(0f, 3.21f, 6.23f);
+            Debug.Log(transform.localPosition);
+            //transform.localEulerAngles = new Vector3(12.724f, 0f, 0f);
+            firstPerson = false;
+        }
+        else
+        {
+            transform.localPosition = new Vector3(0f, 0.7f, -1.14f);
+            Debug.Log(transform.localPosition);
+            //transform.localEulerAngles = new Vector3(0f, 0f, 0f);
+            firstPerson = true;
+        }
+        yield return null;
+    }
+}
