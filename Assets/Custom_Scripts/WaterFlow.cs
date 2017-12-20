3ce4ed6aa539fa87e4d8234e77e276e30ea35500
unknown <usrlocal@GA211-FE05.ad.univ-angers.fr>
2017-12-20 18:40:48 +0100

    Ajout d'un waterflow

--- Assets/Custom_Scripts/WaterFlow.cs
+++ Assets/Custom_Scripts/WaterFlow.cs
@@ -1,47 +1,24 @@
 ﻿using UnityEngine;
 
-[RequireComponent(typeof(ParticleSystem))]
-public class RigidbodyParticleWind : MonoBehaviour
+public class WaterFlow : MonoBehaviour
 {
-    public Transform obj;
-    ParticleSystem particlesSystem;
-    ParticleSystem.Particle[] particles;
-    Rigidbody myRigidbody;
 
-    void Start()
+    [SerializeField] private Transform fan;
+    private Rigidbody r;
+    private float distance;
+
+    public void Start()
     {
-        particlesSystem = gameObject.GetComponent<ParticleSystem>();
-        particles = new ParticleSystem.Particle[1];
-        SetupParticleSystem();
-        myRigidbody = gameObject.GetComponent<Rigidbody>();
+        r = GetComponent<Rigidbody>();
     }
 
-    void FixedUpdate()
+    public void Update()
     {
-        particlesSystem.GetParticles(particles);
+        distance = Vector3.Distance(fan.position, transform.position);
+        if (distance < 50.0f)
+        {
 
-        myRigidbody.velocity += particles[0].velocity;
-        particles[0].position = myRigidbody.position;
-        particles[0].velocity = Vector3.zero;
-
-        particlesSystem.SetParticles(particles, 1);
-    }
-
-    void SetupParticleSystem()
-    {
-        particlesSystem.startLifetime = Mathf.Infinity;
-        particlesSystem.startSpeed = 0;
-        particlesSystem.simulationSpace = ParticleSystemSimulationSpace.World;
-        particlesSystem.maxParticles = 1;
-        particlesSystem.emissionRate = 1;
-        //cant set the following with code so you need to do it manually
-        //1 - Enable "External Forces"
-        //2 - Disable "Renderer"
-
-        //the below is to start the particle at the center
-        particlesSystem.Emit(1);
-        particlesSystem.GetParticles(particles);
-        particles[0].position = Vector3.zero;
-        particlesSystem.SetParticles(particles, 1);
+            r.AddForce(Vector3.down*10000.0f / distance);
+        }
     }
 }
